51d90ee8d1ae8318e343252aac1ec8fbd4532926
Iulius Gutberlet <iulius@sniggle.me>
2015-10-24 19:02:03 +0200

    async module started

---
+++ matemonkey4j-async/src/main/java/me/sniggle/matemonkey4j/BaseMateMonkeyCallable.java
@@ -0,0 +1,7 @@
+package me.sniggle.matemonkey4j;
+
+/**
+ * Created by iulius on 24/10/15.
+ */
+public class BaseMateMonkeyCallable {
+}
---
+++ matemonkey4j-async/src/main/java/me/sniggle/matemonkey4j/dealer/AllDealersCallable.java
@@ -0,0 +1,7 @@
+package me.sniggle.matemonkey4j.dealer;
+
+/**
+ * Created by iulius on 24/10/15.
+ */
+public class AllDealersCallable {
+}
